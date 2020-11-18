6f46477bac15e7a429b9ab3682bf7620786d3e87
apaul <apaul@opentrends.net>
2020-11-18 20:08:33 +0530

    basic functionality completed

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  @Input() fileName = "";
+  @Input() currentPage: number;
+  @Input() totalPages: number;
+  @Input() zoomAmt: number;
+  @Input() zoomMax: number;
+  @Input() zoomMin: number;
 
-  constructor() { }
+  @Output() setZoom: EventEmitter<any> = new EventEmitter();
+  @Output() download: EventEmitter<any> = new EventEmitter();
+  @Output() print: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDownload(event: any): void {
+    this.download.emit();
   }
 
+  onPrint(event: any): void {
+    this.print.emit();
+  }
+
+  zoom(type: string): void {
+    this.setZoom.emit(type);
+  }
 }
--- src/app/container/output-pdf/output-pdf.component.ts
+++ src/app/container/output-pdf/output-pdf.component.ts
@@ -1,14 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PDFDocumentProxy } from "ng2-pdf-viewer";
 
 @Component({
-  selector: 'app-output-pdf',
-  templateUrl: './output-pdf.component.html',
-  styleUrls: ['./output-pdf.component.scss'],
+  selector: "app-output-pdf",
+  templateUrl: "./output-pdf.component.html",
+  styleUrls: ["./output-pdf.component.scss"],
 })
 export class OutputPdfComponent implements OnInit {
-  pdfSrc = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf'; // this sample, dynamic one we will generate with the pdfmake
+  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf"; // this sample, dynamic one we will generate with the pdfmake
   pageVariable = 1;
+
+  // Initialize variables required for the header and this component
+  fileName = "test-document.pdf";
+  // set zoom variables
+  zoom = 0.98; // default initial zoom value
+  zoomMax = 2; // max zoom value
+  zoomMin = 0.5; // min zoom value
+  zoomAmt = 0.2; // stepping zoom values on button click
+  zoomScale = "page-width"; // zoom scale based on the page-width
+  totalPages = 0; // indicates the total number of pages in the pdf document
+  pdf: PDFDocumentProxy; // to access pdf information from the pdf viewer
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  // zoom functionality for the pdf viewer
+  setZoom(type: string): void {
+    console.log(type);
+    if (type === "increment") {
+      this.zoom += this.zoomAmt;
+    } else if (type === "decrement") {
+      this.zoom -= this.zoomAmt;
+    }
+  }
+
+  // pdfSrc value we are taking from the pdfmake generate function in buffer type so currently this willnot work
+  // after the pdf is generated it will work
+  // Download functionality of the pdf
+  download(): void {
+    const blob = new Blob([this.pdfSrc], { type: "application/pdf" });
+
+    // IE doesn't allow using a blob object directly as link href
+    // instead it is necessary to use msSaveOrOpenBlob
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(blob);
+      return;
+    }
+
+    const data = window.URL.createObjectURL(blob);
+    const link = document.createElement("a"); // creating an anchor tag
+    link.href = data; // setting href value to anchor
+    link.download = this.fileName; // giving the download attr to the anchor with the filename that we are giving
+    link.click(); // fake click using the js to download it.
+
+    // For firefox it is necessary to delay revoking the ObjectURl
+    setTimeout(() => {
+      window.URL.revokeObjectURL(data);
+    }, 100);
+  }
+
+  // pdfSrc value we are taking from the pdfmake generate function in buffer type so currently this willnot work
+  // after the pdf is generated it will work
+  // Print functionlaity of the pdf
+  print(): void {
+    // Remove previously added iframes
+    const prevFrames = document.querySelectorAll('iframe[name="pdf-frame"]');
+    if (prevFrames.length) {
+      prevFrames.forEach((item) => item.remove());
+    }
+    // just like download , we are using the blob
+    const blob = new Blob([this.pdfSrc], { type: "application/pdf" });
+    const objectURl = URL.createObjectURL(blob);
+
+    // create iframe element in dom
+    const frame = document.createElement("iframe");
+    frame.style.display = "none"; // hiding the iframe
+    frame.src = objectURl; // setting the source for that iframe
+    // appending this iframe to body
+    document.body.appendChild(frame);
+    frame.name = "pdf-frame";
+    frame.focus();
+
+    // in edge or IE we are using different methods to print
+    if (this.isIE() || this.isEdge()) {
+      frame.contentWindow.document.execCommand("print", false, null);
+    } else {
+      // all other browsers will use this method
+      frame.contentWindow.print();
+    }
+  }
+
+  // to know the browser is IE or not
+  isIE(): boolean {
+    return navigator.userAgent.lastIndexOf("MSIE") !== -1;
+  }
+
+  // to know the browser is Edge or not
+  isEdge(): boolean {
+    return !this.isIE() && !!window.StyleMedia;
+  }
+
+  // after load complete of the pdf function
+  afterLoadComplete(pdf: PDFDocumentProxy): void {
+    this.pdf = pdf;
+    this.totalPages = pdf.numPages;
+  }
 }
